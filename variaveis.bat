b61a52c7c8aace26402f6b0c8912379e5da39e87
Caique Neves <caique.sire@gmail.com>
2025-02-03 17:09:27 -0300

    ajustando variaveis

---
+++ variaveis.bat
@@ -0,0 +1,11 @@
+@echo off
+:: Definir variáveis de ambiente
+set api_key=""
+
+:: Exibir as variáveis para confirmação
+echo Variáveis de ambiente configuradas:
+echo api_key=%api_key%
+
+
+:: Pausa para visualizar a saída antes de fechar
+pause
